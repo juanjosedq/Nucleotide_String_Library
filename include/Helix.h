c49868f62cdb59f591adbc0a013506f6541a872f
DavidM <davidm251008@gmail.com>
2015-11-17 14:03:33 -0600

    Added GPL License

--- include/Helix.h
+++ include/Helix.h
@@ -1,11 +1,32 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+
 #ifndef HELIX_H
 #define HELIX_H
 
 #include "NucleoString.h"
 #include "NucleoString.cpp"
-typedef bool WHICH
+
 #define FIRST 0
 #define SECOND 1
+
+typedef bool WHICH
+
 class Helix{
     private:
         NucleoString<DNA> first;
--- include/NucleoString.h
+++ include/NucleoString.h
@@ -1,3 +1,21 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ */
+
 #ifndef NUCLEOSTRING_H
 #define NUCLEOSTRING_H
 
@@ -9,8 +27,12 @@
 #define RNA 1
 #define ADN 0
 #define ARN 1
-typedef bool NTYPE;
+
+typedef bool NTYPE;// Nuclotid Type
+typedef char NBASE; //NitroBase
+
 using namespace std;
+
 template<NTYPE ANN>
 //! NucleoString Class.
 /*! This class implements a single DNA or RNA chain.
@@ -40,7 +62,7 @@ class NucleoString{
         NucleoString(const NucleoString<ANN> &);
         //! NucleoString<RNA> param constructor.
         /*!
-        * Acts like a copy constructor if the type matches(Both of them are ARN chains). If not
+        * Acts like a copy constructor if the type matches(Both of them are RNA chains). If not
         * it will create a new DNA chain by finding the RNA's transcription.
         * Warning: You may end up creating a DNA chain from a RNA chain if you're not careful enough.
         */
@@ -55,7 +77,7 @@ class NucleoString{
         //! Return the Nucleotid String as an string of nucleobases for the current NucleoString
         string chainret(void);//Falta Implementar
         //! Return the character value of the i nucleobase of the string.
-        char operator[](void);//Falta Implementar.
+        NBASE operator[](void);//Falta Implementar.
         //! Finds and creates a new NucleoString based on the complement of this NucleoString.
         /*!
         * This only creates the complement of the same type of NucleoString. If you want to create
