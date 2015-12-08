41ceee8568b40ed92d231c09f65445b42b75c51d
jjdelgadoq <juanjose.dq@gmail.com>
2015-12-08 03:24:08 -0600

    clase Management01

---
+++ src/Management 01.cpp
@@ -0,0 +1,23 @@
+#include "Management.hh"
+
+
+//Funcion constructora va vacia pero no se si hay que establecerla...
+Management::Management(){
+{
+
+void management:txt(string titulo, NucleoString){
+	ofstream fs(titulo".txt");
+	fs <<">"titulo"\n" << endl;
+	fs << NucleoString << endl; 
+}
+
+
+management:loadString(string file){
+	ifstream fe(file".text");
+	fe.getline(fe,line);
+	string nucleotide = fe.getline(fe,line);
+	return nucleotide;
+}
+
+
+
